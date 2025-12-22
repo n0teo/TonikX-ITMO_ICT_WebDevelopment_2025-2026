c3edf7c2ed8220101d77fe33c43f59fec4ef4bac
n0teo <apleev2018@mail.ru>
2025-12-22 07:07:52 +0300

    очередная попытка воскресить MkDocs

--- students/K3339/Apleev_Dmitry/lab1/task1/client.py
+++ students/K3339/Apleev_Dmitry/lab1/task1/client.py
@@ -4,7 +4,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(5)
 
 try:
-    mes = input("Введите сообщение для сервера: ")
+    mes = input("Введите сообщение для  сервера: ")
     client_socket.sendto(mes.encode('utf-8'), ('localhost', 12345))
     data, addr = client_socket.recvfrom(1024)
     print(f"Ответ от сервера: {data.decode('utf-8')}")
